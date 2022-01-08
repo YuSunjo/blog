da7cdca4166dadd1ea21e1ed0d65cff03f06fcd4
YuSunjo <tnswh2023@gmail.com>
2022-01-08 17:30:18 +0900

    feat(member): 인터셉터 수정

--- blog-api/src/main/kotlin/com/blog/config/auth/MemberComponent.kt
+++ blog-api/src/main/kotlin/com/blog/config/auth/MemberComponent.kt
@@ -1,7 +1,6 @@
 package com.blog.config.auth
 
 import com.blog.config.jwt.JwtTokenProvider
-import com.blog.domain.admin.repository.AdminRepository
 import com.blog.domain.member.repository.MemberRepository
 import com.blog.exception.JwtException
 import com.blog.exception.NotFoundException
@@ -19,13 +18,12 @@ class MemberComponent(
     fun authCheck(request: HttpServletRequest): Boolean {
         val header = request.getHeader(HttpHeaders.AUTHORIZATION)
         if (!StringUtils.hasLength(header)) {
-            throw ValidationException("잘못된 헤더 ${header} 입니다.")
+            throw ValidationException("잘못된 헤더 $header 입니다.")
         }
 
-        val memberId = jwtTokenProvider.getSubject(header)
-        if (memberId == null) {
-            throw JwtException("jwt body에 아무것도 존재하지 않습니다.")
-        }
+        val memberId = jwtTokenProvider.getSubject(header) ?: throw JwtException("jwt body 에 아무것도 존재하지 않습니다.")
+        memberRepository.findMemberByIdIsAdmin(memberId.toLong())
+            ?: throw NotFoundException("존재하지 않는 멤버 아이디 $memberId 입니다")
         request.setAttribute("memberId", memberId.toLong())
         return true
     }
