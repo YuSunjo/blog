b1e926ac68f11090ab90f1b24026a0bf13ad82da
YuSunjo <tnswh2023@gmail.com>
2022-01-23 17:24:57 +0900

    feat(comment): comment 관련 api 작성

--- blog-api/src/main/kotlin/com/blog/controller/comment/CommentController.kt
+++ blog-api/src/main/kotlin/com/blog/controller/comment/CommentController.kt
@@ -1,13 +1,38 @@
 package com.blog.controller.comment
 
+import com.blog.ApiResponse
+import com.blog.config.argument.MemberId
+import com.blog.config.auth.Member
+import com.blog.dto.comment.CreateCommentRequest
+import com.blog.dto.comment.UpdateCommentRequest
+import com.blog.dto.comment.response.CommentInfoResponse
 import com.blog.service.comment.CommentService
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 class CommentController(
     private val commentService: CommentService
 ) {
     // api 에서는 get 만
+    @Member
+    @PostMapping("api/v1/admin/comment")
+    fun createComment(@RequestBody @Valid request: CreateCommentRequest, @MemberId memberId: Long): ApiResponse<String> {
+        commentService.createComment(request, memberId)
+        return ApiResponse.OK
+    }
 
+    @Member
+    @PutMapping("api/v1/admin/comment")
+    fun updateComment(@RequestBody @Valid request: UpdateCommentRequest, @MemberId memberId: Long): ApiResponse<String> {
+        commentService.updateComment(request, memberId)
+        return ApiResponse.OK
+    }
+
+    @Member
+    @GetMapping("api/v1/admin/comment/{boardId}")
+    fun retrieveComment(@PathVariable boardId: Long, @MemberId memberId: Long): ApiResponse<List<CommentInfoResponse>> {
+        return ApiResponse.success(commentService.retrieveComment(boardId, memberId))
+    }
 
 }
--- blog-api/src/main/kotlin/com/blog/service/comment/CommentService.kt
+++ blog-api/src/main/kotlin/com/blog/service/comment/CommentService.kt
@@ -1,10 +1,62 @@
 package com.blog.service.comment
 
+import com.blog.domain.board.repository.BoardRepository
+import com.blog.domain.comment.Comment
 import com.blog.domain.comment.repository.CommentRepository
+import com.blog.domain.member.Member
+import com.blog.domain.member.repository.MemberRepository
+import com.blog.dto.comment.CreateCommentRequest
+import com.blog.dto.comment.UpdateCommentRequest
+import com.blog.dto.comment.response.CommentInfoResponse
+import com.blog.exception.NotFoundException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.stream.Collectors
 
 @Service
 class CommentService(
-    private val commentRepository: CommentRepository
+    private val boardRepository: BoardRepository,
+    private val commentRepository: CommentRepository,
+    private val memberRepository: MemberRepository
 ) {
+
+    @Transactional
+    fun createComment(request: CreateCommentRequest, memberId: Long) {
+        val board = (boardRepository.findBoardById(request.boardId)
+            ?: throw NotFoundException("존재하지 않는 게시글 ${request.boardId} 입니다."))
+        (memberRepository.findMemberByIdIsAdmin(memberId)
+            ?: throw NotFoundException("존재하지 않는 멤버 $memberId 입니다."))
+        if (request.parentCommentId != null) {
+            val boardComment = (commentRepository.findCommentById(request.parentCommentId!!)
+                ?: throw NotFoundException("존재하지 않는 부모 댓글 ${request.parentCommentId} 입니다."))
+            boardComment.addChildComment(memberId, request.content, request.boardId)
+            board.incrementLikeCount()
+        }
+        commentRepository.save(Comment.newRootComment(request.boardId, request.content, memberId))
+        board.incrementLikeCount()
+    }
+
+    @Transactional
+    fun updateComment(request: UpdateCommentRequest, memberId: Long) {
+        val comment = (commentRepository.findCommentById(request.commentId)
+            ?: throw NotFoundException("존재하지 않는 댓글 ${request.commentId} 입니다."))
+        (memberRepository.findMemberByIdIsAdmin(memberId)
+            ?: throw NotFoundException("존재하지 않는 멤버 $memberId 입니다."))
+        comment.updateContent(request.content)
+    }
+
+    @Transactional
+    fun retrieveComment(boardId: Long, memberId: Long): List<CommentInfoResponse> {
+        (boardRepository.findBoardById(boardId)
+            ?: throw NotFoundException("존재하지 않는 게시글 $boardId 입니다."))
+        (memberRepository.findMemberByIdIsAdmin(memberId)
+            ?: throw NotFoundException("존재하지 않는 멤버 $memberId 입니다."))
+        val boardCommentList = commentRepository.findCommentByBoardId(boardId)
+        val commentMemberIds = boardCommentList.stream().map { it.memberId }.collect(Collectors.toList())
+        val memberMap: Map<Long, Member> = memberRepository.findAllByIds(commentMemberIds).associateBy { it.id }
+        return boardCommentList.stream().map {
+            CommentInfoResponse.of(it, memberMap)
+        }.collect(Collectors.toList())
+    }
+
 }
