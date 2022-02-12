b616aecf57769862ee41eacaedc22018584e2b38
YuSunjo <tnswh2023@gmail.com>
2022-02-12 13:43:22 +0900

    feat(sentry): sentry 설정

--- blog-common/src/main/kotlin/com/blog/exception/controllerAdvice/ExceptionController.kt
+++ blog-common/src/main/kotlin/com/blog/exception/controllerAdvice/ExceptionController.kt
@@ -6,6 +6,7 @@ import com.blog.exception.JwtException
 import com.blog.exception.NotFoundException
 import com.blog.exception.ValidationException
 import com.blog.exception.errorCode.ErrorCode
+import io.sentry.Sentry
 import org.springframework.http.HttpStatus
 import org.springframework.validation.BindException
 import org.springframework.web.bind.MethodArgumentNotValidException
@@ -19,36 +20,42 @@ class ExceptionController {
     @ExceptionHandler(value = [NotFoundException::class])
     @ResponseStatus(HttpStatus.NOT_FOUND)
     fun handleNotFound(e: NotFoundException): ErrorResponse {
+        Sentry.captureException(e)
         return ErrorResponse.error(e.errorCode, e.localizedMessage)
     }
 
     @ExceptionHandler(value = [ConflictException::class])
     @ResponseStatus(HttpStatus.CONFLICT)
     fun handlerConflict(e: ConflictException): ErrorResponse {
+        Sentry.captureException(e)
         return ErrorResponse.error(e.errorCode, e.localizedMessage)
     }
 
     @ExceptionHandler(value = [JwtException::class])
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     fun handlerJwt(e: JwtException): ErrorResponse {
+        Sentry.captureException(e)
         return ErrorResponse.error(e.errorCode, e.localizedMessage)
     }
 
     @ExceptionHandler(value = [ValidationException::class])
     @ResponseStatus(HttpStatus.CONFLICT)
     fun handlerValidation(e: ValidationException): ErrorResponse {
+        Sentry.captureException(e)
         return ErrorResponse.error(e.errorCode, e.localizedMessage)
     }
 
     @ExceptionHandler(value = [MethodArgumentNotValidException::class])
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handlerValidation(e: MethodArgumentNotValidException): ErrorResponse {
+        Sentry.captureException(e)
         return ErrorResponse.error(ErrorCode.VALIDATION_EXCEPTION, e.bindingResult.allErrors.get(0).defaultMessage)
     }
 
     @ExceptionHandler(value = [BindException::class])
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handlerValidation(e: BindException): ErrorResponse {
+        Sentry.captureException(e)
         return ErrorResponse.error(ErrorCode.VALIDATION_EXCEPTION, e.localizedMessage)
     }
 
