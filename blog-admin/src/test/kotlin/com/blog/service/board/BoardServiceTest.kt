9606363ab440078305951a1b05d1d390d0fe6872
ballde <67853463+YuSunjo@users.noreply.github.com>
2022-06-21 16:32:34 +0900

    test(retrieveBoard): 테스트 실패하는 부분 해결

--- blog-admin/src/test/kotlin/com/blog/service/board/BoardServiceTest.kt
+++ blog-admin/src/test/kotlin/com/blog/service/board/BoardServiceTest.kt
@@ -10,6 +10,7 @@ import com.blog.dto.board.UpdateBoardRequest
 import com.blog.service.TestUtils
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -84,6 +85,7 @@ class BoardServiceTest(
         TestUtils.assertBoardInfoResponse(response, board.title, board.content, board.boardThumbnailUrl)
     }
 
+    @DisplayName("특정 조건 없이 게시글 불러오기")
     @Test
     fun retrieveBoard() {
         // given
@@ -96,7 +98,32 @@ class BoardServiceTest(
         board3.addHashTag(Arrays.asList("1", "2"), 1)
 
         boardRepository.saveAll(listOf(board1, board2, board3))
-        val request = RetrieveBoardRequest(1, 3, null, null)
+        val request = RetrieveBoardRequest(1, 3, null, null, null)
+
+        // when
+        val response = boardService.retrieveBoard(request)
+
+        // then
+        val boardList = boardRepository.findAll()
+        assertThat(boardList).hasSize(3)
+        assertThat(response.boardList).hasSize(3)
+        assertThat(response.totalPage).isEqualTo(2)
+    }
+
+    @DisplayName("category를 통해서 게시글 불러오기")
+    @Test
+    fun retrieveBoard_2() {
+        // given
+        val category = categoryRepository.save(Category("gogo"))
+        val board1 = Board("title1", "content1", false, "url1", category, 1L, 0, 0)
+        board1.addHashTag(Arrays.asList("1", "2"), 1)
+        val board2 = Board("title2", "content2", false, "url2", category, 1L, 0, 0)
+        board2.addHashTag(Arrays.asList("1", "2"), 1)
+        val board3 = Board("title3", "content3", false, "url3", category, 1L, 0, 0)
+        board3.addHashTag(Arrays.asList("1", "2"), 1)
+
+        boardRepository.saveAll(listOf(board1, board2, board3))
+        val request = RetrieveBoardRequest(1, 3, null, "gogo", listOf("1"))
 
         // when
         val response = boardService.retrieveBoard(request)
@@ -109,6 +136,86 @@ class BoardServiceTest(
         TestUtils.assertBoardInfoResponse(response.boardList[0], board3.title, board3.content, board3.boardThumbnailUrl)
         TestUtils.assertBoardInfoResponse(response.boardList[1], board2.title, board2.content, board2.boardThumbnailUrl)
         TestUtils.assertBoardInfoResponse(response.boardList[2], board1.title, board1.content, board1.boardThumbnailUrl)
+    }
+
+    @DisplayName("hashTagList를 통해서 게시글 불러오기")
+    @Test
+    fun retrieveBoard_3() {
+        // given
+        val category = categoryRepository.save(Category("gogo"))
+        val board1 = Board("title1", "content1", false, "url1", category, 1L, 0, 0)
+        board1.addHashTag(Arrays.asList("생일", "기념"), 1)
+        val board2 = Board("title2", "content2", false, "url2", category, 1L, 0, 0)
+        board2.addHashTag(Arrays.asList("강릉"), 1)
+        val board3 = Board("title3", "content3", false, "url3", category, 1L, 0, 0)
+        board3.addHashTag(Arrays.asList("생일", "강릉"), 1)
+
+        boardRepository.saveAll(listOf(board1, board2, board3))
+        val request = RetrieveBoardRequest(1, 3, null, null, listOf("강릉"))
+
+        // when
+        val response = boardService.retrieveBoard(request)
+
+        // then
+        val boardList = boardRepository.findAll()
+        assertThat(boardList).hasSize(3)
+        assertThat(response.boardList).hasSize(2)
+        assertThat(response.totalPage).isEqualTo(1)
+        TestUtils.assertBoardInfoResponse(response.boardList[0], board3.title, board3.content, board3.boardThumbnailUrl)
+        TestUtils.assertBoardInfoResponse(response.boardList[1], board2.title, board2.content, board2.boardThumbnailUrl)
+    }
+
+    @DisplayName("title search 조건으로 게시글 불러오기")
+    @Test
+    fun retrieveBoard_4() {
+        // given
+        val category = categoryRepository.save(Category("gogo"))
+        val board1 = Board("title1", "content1", false, "url1", category, 1L, 0, 0)
+        board1.addHashTag(Arrays.asList("생일", "기념"), 1)
+        val board2 = Board("title2", "content2", false, "url2", category, 1L, 0, 0)
+        board2.addHashTag(Arrays.asList("강릉"), 1)
+        val board3 = Board("title3", "content3", false, "url3", category, 1L, 0, 0)
+        board3.addHashTag(Arrays.asList("생일", "강릉"), 1)
+
+        boardRepository.saveAll(listOf(board1, board2, board3))
+        val request = RetrieveBoardRequest(1, 3, "ti", null, listOf("강릉"))
+
+        // when
+        val response = boardService.retrieveBoard(request)
+
+        // then
+        val boardList = boardRepository.findAll()
+        assertThat(boardList).hasSize(3)
+        assertThat(response.boardList).hasSize(2)
+        assertThat(response.totalPage).isEqualTo(1)
+        TestUtils.assertBoardInfoResponse(response.boardList[0], board3.title, board3.content, board3.boardThumbnailUrl)
+        TestUtils.assertBoardInfoResponse(response.boardList[1], board2.title, board2.content, board2.boardThumbnailUrl)
+    }
+
+    @DisplayName("title search 조건으로 게시글 불러오기")
+    @Test
+    fun retrieveBoard_5() {
+        // given
+        val category = categoryRepository.save(Category("gogo"))
+        val board1 = Board("title1", "content1", false, "url1", category, 1L, 0, 0)
+        board1.addHashTag(Arrays.asList("생일", "기념"), 1)
+        val board2 = Board("title2", "content2", false, "url2", category, 1L, 0, 0)
+        board2.addHashTag(Arrays.asList("강릉"), 1)
+        val board3 = Board("title3", "content3", false, "url3", category, 1L, 0, 0)
+        board3.addHashTag(Arrays.asList("생일", "강릉"), 1)
+
+        boardRepository.saveAll(listOf(board1, board2, board3))
+        val request = RetrieveBoardRequest(1, 3, "3", null, listOf("강릉"))
+
+        // when
+        val response = boardService.retrieveBoard(request)
+
+        // then
+        val boardList = boardRepository.findAll()
+        assertThat(boardList).hasSize(3)
+        assertThat(response.boardList).hasSize(1)
+        assertThat(response.totalPage).isEqualTo(1)
+        TestUtils.assertBoardInfoResponse(response.boardList[0], board3.title, board3.content, board3.boardThumbnailUrl)
     }
 
 }
