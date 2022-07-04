43062b28d4d8c443412546049a1a5ca038015c3e
ballde <67853463+YuSunjo@users.noreply.github.com>
2022-07-04 10:15:18 +0900

    feat(test): - kotest 해보기(BehaviorSpec)

--- blog-admin/src/test/kotlin/com/blog/adminService/category/CategoryServiceKoTest.kt
+++ blog-admin/src/test/kotlin/com/blog/adminService/category/CategoryServiceKoTest.kt
@@ -1,25 +1,48 @@
 package com.blog.adminService.category
 
+import com.blog.domain.category.Category
 import com.blog.domain.category.repository.CategoryRepository
 import com.blog.dto.category.CreateCategoryRequest
 import com.blog.service.category.CategoryService
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.every
 import io.mockk.mockk
 
-//val categoryRepository: CategoryRepository = mockk()
-
-//val categoryService: CategoryService = CategoryService(categoryRepository)
-
 class CategoryServiceKoTest : BehaviorSpec({
-//    Given("요청이 들어오는 상태에서") {
-//        val request = CreateCategoryRequest("gogo")
-//        When("카테고리 생성을 하게 되면") {
-//            categoryService.createCategory(request)
-//            val categoryList = categoryRepository.findAll()
-//            Then("db에 카테고리는 1개가 있다.") {
-//                categoryList.size shouldBe 1
-//            }
-//        }
-//    }
+
+    val categoryRepository = mockk<CategoryRepository>(relaxed = true, relaxUnitFun = true)
+    val categoryService = CategoryService(categoryRepository)
+
+    afterContainer {
+        clearAllMocks()
+    }
+
+    Given("요청이 들어오는 상태에서") {
+        val request = CreateCategoryRequest("gogo")
+        When("카테고리 생성을 하게 되면") {
+            every {
+                categoryService.createCategory(request)
+            }
+            Then("db에 저장된 카테고리와 내가 적은 카테고리의 이름이 같다.") {
+                val category = getCategory(request.categoryName)
+                every {
+                    categoryRepository.findCategoryById(1L)
+                } returns category
+                category.categoryName shouldBe request.categoryName
+            }
+            Then("db에 카테고리는 1개가 있다.") {
+                val categoryListSize = 1
+                every {
+                    categoryRepository.findAll().size
+                } returns categoryListSize
+                categoryListSize shouldBe 1
+            }
+        }
+    }
 })
+
+fun getCategory(categoryName: String): Category {
+    return Category(categoryName)
+}
