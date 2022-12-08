33aa8a1870a2d2bdffa5c66eb32d91d6ef5b529d
ballde <67853463+YuSunjo@users.noreply.github.com>
2022-12-08 16:07:24 +0900

    feat(port): port 변경

--- deploy/deploy-docker.sh
+++ deploy/deploy-docker.sh
@@ -7,7 +7,7 @@ ADMIN_DOCKER_NAME=blog-admin
 cat ~/deploy/GIT-TOKEN.txt | docker login ghcr.io -u yusunjo --password-stdin
 
 docker stop $API_DOCKER_NAME && docker rm -fv $API_DOCKER_NAME && docker rmi -f $API_DOCKER_IMAGE:latest
-docker run -d -p 8888:8888 --name $API_DOCKER_NAME --restart always $API_DOCKER_IMAGE:latest
+docker run -d -p 8080:8080 --name $API_DOCKER_NAME --restart always $API_DOCKER_IMAGE:latest
 
 docker stop $ADMIN_DOCKER_NAME && docker rm -fv $ADMIN_DOCKER_NAME && docker rmi -f $ADMIN_DOCKER_IMAGE:latest
-docker run -d -p 9999:9999 --name $ADMIN_DOCKER_NAME --restart always $ADMIN_DOCKER_IMAGE:latest
+docker run -d -p 9000:9000 --name $ADMIN_DOCKER_NAME --restart always $ADMIN_DOCKER_IMAGE:latest
